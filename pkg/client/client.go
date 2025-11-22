eca0ef42eaf96531cb30d140cce3ccfdf96397e3
alexciechonski <alex.ciechonski@gmail.com>
2025-11-22 19:50:26 +0100

    added full github paths to imports

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	"time"
 
-	"bigtablelite/proto"
+	"https://github.com/alexciechonski/BigTableLite/proto"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
