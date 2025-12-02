ae6890b18ae4abe39e8190a44053abb06b0f44ae
alexciechonski <alex.ciechonski@gmail.com>
2025-12-02 02:36:48 +0100

    started a benchmark file

---
+++ pkg/wal/wal_bench_test.go
@@ -0,0 +1,18 @@
+package wal
+
+import (
+	"os"
+	"testing"
+)
+
+func BenchmarkSerializeOperation(b *testing.B) {
+	key := []byte("my-key")
+	value := []byte("my-value-1234567890")
+
+	for i := 0; i < b.N; i++ {
+		_, err := SerializeOperation("set", key, value)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
