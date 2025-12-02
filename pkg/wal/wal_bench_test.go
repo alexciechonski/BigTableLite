9469f8ba868fe2acdcbe2443cd44ade6494fbf2f
alexciechonski <alex.ciechonski@gmail.com>
2025-12-02 02:39:13 +0100

    added a package benchmark

--- pkg/wal/wal_bench_test.go
+++ pkg/wal/wal_bench_test.go
@@ -2,6 +2,7 @@ package wal
 
 import (
 	"testing"
+	"os"
 )
 
 func BenchmarkSerializeOperation(b *testing.B) {
@@ -11,6 +12,25 @@ func BenchmarkSerializeOperation(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, err := SerializeOperation("set", key, value)
 		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkWalAppend(b *testing.B) {
+	tmpfile := "wal_bench.txt"
+	os.Remove(tmpfile)
+	w, err := NewWal(tmpfile)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer w.Close()
+
+	entry, _ := SerializeOperation("set", []byte("k"), []byte("value"))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := w.Append(entry); err != nil {
 			b.Fatal(err)
 		}
 	}
