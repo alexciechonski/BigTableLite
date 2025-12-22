a75979b25367c5a648e8f1492a0db0e3f21bdd85
alexciechonski <alex.ciechonski@gmail.com>
2025-12-22 08:57:01 +0100

    fixed all tests

--- pkg/server/server_bench_test.go
+++ pkg/server/server_bench_test.go
@@ -1,63 +1,58 @@
 package server
 
 import (
-	"context"
-	"os"
-	"testing"
+    "context"
+    "os"
+    "testing"
 
-	"github.com/alexciechonski/BigTableLite/proto"
-	"github.com/go-redis/redismock/v9"
-	"github.com/redis/go-redis/v9"
+    "github.com/alexciechonski/BigTableLite/proto"
+    "github.com/go-redis/redismock/v9"
+    "github.com/redis/go-redis/v9"
 )
 
 func newRedisForBenchmark() (*redis.Client, redismock.ClientMock) {
-	if os.Getenv("GITHUB_ACTIONS") == "true" {
-		return redismock.NewClientMock()
-	}
+    if os.Getenv("GITHUB_ACTIONS") == "true" {
+        return redismock.NewClientMock()
+    }
 
-	return redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	}), nil
+    return redis.NewClient(&redis.Options{
+        Addr: "localhost:6379",
+    }), nil
 }
 
 func BenchmarkServerRedisSet(b *testing.B) {
-	ctx := context.Background()
+    ctx := context.Background()
+    rdb, mock := newRedisForBenchmark()
 
-	rdb, mock := newRedisForBenchmark()
+    if mock != nil {
+        mock.ExpectSet("bench-key", "bench-value", 0).SetVal("OK")
+    }
 
-	if mock != nil {
-		mock.ExpectSet("bench-key", "bench-value", 0).SetVal("OK")
-	}
+    server := &BigTableLiteServer{
+        redis: rdb, // Changed from redisClient to redis
+    }
 
-	server := &BigTableLiteServer{
-		redisClient: rdb,
-		useRedis:    true,
-	}
+    req := &proto.SetRequest{
+        Key:   "bench-key",
+        Value: "bench-value",
+    }
 
-	req := &proto.SetRequest{
-		Key:   "bench-key",
-		Value: "bench-value",
-	}
-
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		server.Set(ctx, req)
-	}
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        server.Set(ctx, req)
+    }
 }
 
 func BenchmarkRedisDirectSet(b *testing.B) {
-	ctx := context.Background()
+    ctx := context.Background()
+    rdb, mock := newRedisForBenchmark()
 
-	rdb, mock := newRedisForBenchmark()
+    if mock != nil {
+        mock.ExpectSet("bench-key", "bench-value", 0).SetVal("OK")
+    }
 
-	if mock != nil {
-		mock.ExpectSet("bench-key", "bench-value", 0).SetVal("OK")
-	}
-
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		rdb.Set(ctx, "bench-key", "bench-value", 0)
-	}
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        rdb.Set(ctx, "bench-key", "bench-value", 0)
+    }
 }
